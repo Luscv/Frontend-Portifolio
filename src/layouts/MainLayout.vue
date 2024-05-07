4bf3ff246fb245d941498a5eb1d5f35906bf9037
LucasBarros <lucas.costa@defensoria.mg.def.br>
2024-05-07 15:02:53 -0300

    layout

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -2,15 +2,7 @@
   <q-layout view="hHh lpR fFf" class="bg-grey-9 q-pa-xl">
     <div class="justify-center q-my-xl row q-col-gutter-md">
       <q-card class="col-lg-2 col-sm-6 col-xs-12 q-mt-md">
-        <q-avatar
-          rounded
-          size="100px"
-          font-size="82px"
-          color="teal"
-          text-color="white"
-          icon="directions"
-        >
-        </q-avatar>
+        <ProfileComponent/>
       </q-card>
 
       <q-page-container class="col-lg-9 col-sm-6 col-xs-12">
@@ -22,16 +14,6 @@
   </q-layout>
 </template>
 
-<script lang="ts">
-import { defineComponent } from 'vue';
-
-export default defineComponent({
-  name: 'MainLayout',
-
-  components: {},
-
-  setup() {
-    return {};
-  },
-});
+<script setup lang="ts">
+import  ProfileComponent  from '../view/components/ProfileComponent.vue'
 </script>
---
+++ src/view/components/ProfileComponent.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="">
+        <div class="">
+            <q-avatar 
+            size="100px"
+            rounded
+        >
+            <img src="https://cdn.quasar.dev/img/avatar4.jpg">
+            </q-avatar>
+            <div>Nome</div>
+            <q-badge>Cargo</q-badge>
+        </div>
+        
+        <q-separator/>
+        <div>
+            <q-card>personal</q-card>
+            <div>socials</div>
+        </div>
+    </div>
+    
+</template>
+<script setup lang="ts">
+
+</script>
