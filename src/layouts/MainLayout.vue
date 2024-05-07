5dd18c1a46b0a2b033a80da4a76b8d2a16b5a97e
LucasBarros <lucas.costa@defensoria.mg.def.br>
2024-05-07 16:26:48 -0300

    profile-info-styling

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,12 +1,12 @@
 <template>
-  <q-layout view="hHh lpR fFf" class="bg-grey-9 q-pa-xl">
+  <q-layout view="hHh lpR fFf" class="bg-dark q-pa-xl">
     <div class="justify-center q-my-xl row q-col-gutter-md">
-      <q-card class="col-lg-2 col-sm-6 col-xs-12 q-mt-md">
+      <q-card class="col-lg-2 col-sm-6 col-xs-12 q-mt-md bg-grey-10 text-grey-4">
         <ProfileComponent/>
       </q-card>
 
-      <q-page-container class="col-lg-9 col-sm-6 col-xs-12">
-        <q-card>
+      <q-page-container class="col-lg-9 col-sm-6 col-xs-12 ">
+        <q-card class="bg-grey-10 text-grey-4">
           <router-view />
         </q-card>
       </q-page-container>
--- src/view/components/ProfileComponent.vue
+++ src/view/components/ProfileComponent.vue
@@ -1,24 +1,87 @@
 <template>
-    <div class="">
-        <div class="">
-            <q-avatar 
-            size="100px"
-            rounded
-        >
-            <img src="https://cdn.quasar.dev/img/avatar4.jpg">
-            </q-avatar>
-            <div>Nome</div>
-            <q-badge>Cargo</q-badge>
+    <div class="justify-center">
+        <div class="row justify-center">
+            <div></div>
+            <div class="text-center q-mt-xl">
+                <q-avatar 
+                size="100px"
+                rounded
+                >
+                    <img src="https://cdn.quasar.dev/img/avatar4.jpg">
+                </q-avatar>
+                <div class="q-mt-md">{{personalInfo.name}}</div>
+                <div class="column">
+                    <q-badge class="q-mt-md  text-subtitle2 justify-center" color="grey-9">{{ personalInfo.role }}</q-badge>
+                    <div>
+                        <q-badge class="q-mt-sm q-mx-sm justify-center text-subtitle" color="blue-8">{{ personalInfo.tech }}</q-badge>
+                        <q-badge class="q-mt-sm q-mx-sm justify-center text-subtitle" color="green-8">{{ personalInfo.tech_2 }}</q-badge>
+                    </div>
+                </div>
+                
+            </div>
+            <div></div>
         </div>
+            
         
-        <q-separator/>
-        <div>
-            <q-card>personal</q-card>
+        <q-separator color="grey-9" class="q-mt-md"/>
+        <div class="q-mt-md">
+            <div 
+                v-for="items in personalList" 
+                v-bind:key="items.id"
+                class="row items-center"
+            >
+                <q-card class="bg-grey-10 q-ma-md q-pa-md">
+                    <q-icon :name="items.icon"/>
+                </q-card>
+                <div>
+                    <div>{{ items.title }}</div>
+                    <div>{{ items.data }}</div>
+                </div>
+            </div>
+            
             <div>socials</div>
         </div>
     </div>
     
 </template>
 <script setup lang="ts">
+
+const personalInfo = {
+    name: 'Lucas Barros',
+    age: 24,
+    role: 'Software Engineer',
+    tech: 'React Native',
+    tech_2: 'Vue',
+    city: 'Belo Horizonte',
+    country: 'Brazil',
+    state: 'Minas Gerais',
+}
+
+const personalList = [
+    {
+        id: 1,
+        title: 'email',
+        data: 'email@mail.com',
+        icon: 'fa-email',
+    },
+    {
+        id: 2,
+        title: 'phone',
+        data: '+55 (31) 9 1234-5678',
+        icon: 'fa-phone',
+    },
+    {
+        id: 3,
+        title: 'birthday',
+        data: '12/12/1212',
+        icon: 'fa-event',
+    },
+    {
+        id: 4,
+        title: 'location',
+        data: 'Brazil, MG',
+        icon: 'fa-location-dot',
+    },
+]
 
 </script>
