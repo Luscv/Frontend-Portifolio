da03d51ed29b3de3248c5bf9b34ddcae4e6f33c1
LucasBarros <lucas.costa@defensoria.mg.def.br>
2024-05-07 11:57:58 -0300

    layout-build-finished

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,82 +1,37 @@
 <template>
-  <q-layout view="lHh Lpr lFf">
-    <q-card>alo</q-card>
-
-
-    <q-page-container>
-      <q-card>
-        <router-view />
+  <q-layout view="hHh lpR fFf" class="bg-grey-9 q-pa-xl">
+    <div class="justify-center q-my-xl row q-col-gutter-md">
+      <q-card class="col-lg-2 col-sm-6 col-xs-12 q-mt-md">
+        <q-avatar
+          rounded
+          size="100px"
+          font-size="82px"
+          color="teal"
+          text-color="white"
+          icon="directions"
+        >
+        </q-avatar>
       </q-card>
-    </q-page-container>
+
+      <q-page-container class="col-lg-9 col-sm-6 col-xs-12">
+        <q-card>
+          <router-view />
+        </q-card>
+      </q-page-container>
+    </div>
   </q-layout>
 </template>
 
 <script lang="ts">
-import { defineComponent, ref } from 'vue';
-
-
-const linksList = [
-  {
-    title: 'Docs',
-    caption: 'quasar.dev',
-    icon: 'school',
-    link: 'https://quasar.dev'
-  },
-  {
-    title: 'Github',
-    caption: 'github.com/quasarframework',
-    icon: 'code',
-    link: 'https://github.com/quasarframework'
-  },
-  {
-    title: 'Discord Chat Channel',
-    caption: 'chat.quasar.dev',
-    icon: 'chat',
-    link: 'https://chat.quasar.dev'
-  },
-  {
-    title: 'Forum',
-    caption: 'forum.quasar.dev',
-    icon: 'record_voice_over',
-    link: 'https://forum.quasar.dev'
-  },
-  {
-    title: 'Twitter',
-    caption: '@quasarframework',
-    icon: 'rss_feed',
-    link: 'https://twitter.quasar.dev'
-  },
-  {
-    title: 'Facebook',
-    caption: '@QuasarFramework',
-    icon: 'public',
-    link: 'https://facebook.quasar.dev'
-  },
-  {
-    title: 'Quasar Awesome',
-    caption: 'Community Quasar projects',
-    icon: 'favorite',
-    link: 'https://awesome.quasar.dev'
-  }
-];
+import { defineComponent } from 'vue';
 
 export default defineComponent({
   name: 'MainLayout',
 
-  components: {
-    
+  components: {},
+
+  setup() {
+    return {};
   },
-
-  setup () {
-    const leftDrawerOpen = ref(false)
-
-    return {
-      essentialLinks: linksList,
-      leftDrawerOpen,
-      toggleLeftDrawer () {
-        leftDrawerOpen.value = !leftDrawerOpen.value
-      }
-    }
-  }
 });
 </script>
