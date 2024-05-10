244acc84a1db8690ed9d6fcadfeff33228ca9ebf
LucasBarros <lucas.costa@defensoria.mg.def.br>
2024-05-10 16:09:28 -0300

    tab-styling-done

--- src/view/pages/HomePage.vue
+++ src/view/pages/HomePage.vue
@@ -1,21 +1,28 @@
 <template>
   <q-page>
-    <div class="row justify-end">
-      <q-tabs
-        v-model="tab"
+    <div class="row justify-between">
+      <q-toolbar class="q-pt-lg q-pl-lg items-end" style="width: min-content;">
+        <q-toolbar-title class="text-h3">
+          {{ tab }}
+        </q-toolbar-title>
         
-        active-color="primary"
-        indicator-color="transparent"
-        align="right"
-        class="bg-grey-9 q-py-sm q-px-xl"
-        style="width: max-content; border-bottom-left-radius: 40px;"
-      >
-        <q-tab name="About" label="About"/>
-        <q-tab name="Resume" label="Resume" />
-        <q-tab name="Portifolio" label="Portifolio" />
-        <q-tab name="Contact" label="Contact" />
-      </q-tabs>
+        
+      </q-toolbar>
+      <q-tabs
+          v-model="tab"
+          
+          active-color="primary"
+          indicator-color="transparent"
+          class="bg-grey-9 q-pl-xl q-pr-lg"
+          style="width: max-content; border-bottom-left-radius: 60px;"
+        >
+          <q-tab name="About" label="About"/>
+          <q-tab name="Resume" label="Resume" />
+          <q-tab name="Portifolio" label="Portifolio" />
+          <q-tab name="Contact" label="Contact" />
+        </q-tabs>
     </div>
+      <q-separator color="primary" class="text-center q-mt-md q-ml-lg" style="width: 45px; height: 5px; border-radius: 20px;"/>
       <q-tab-panels class="bg-grey-10" v-model="tab" animated>
           <q-tab-panel name="About">
             <AboutTab/>
