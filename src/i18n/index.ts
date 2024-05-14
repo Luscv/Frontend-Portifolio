0df0638a439dceb2a4341429796889aef337bcef
Luscv <lucbarrospc@gmail.com>
2024-05-14 20:33:34 -0300

    update-deploy

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -1,17 +1,5 @@
-// import { createI18n } from 'vue-i18n';
-// import ptBr from './locales/pt-br.json' assert { type: 'json' };
-// import enUS from './locales/en-US.json' assert { type: 'json' };
+import enUS from './locales/en-US';
 
-// const i18n = createI18n({
-//     legacy: false,
-//     fallbackLocale: 'pt-br',
-//     locale: 'pt-br',
-//     allowComposition: true,
-//     globalInjection: true,
-//     messages: {
-//         'pt-br': ptBr,
-//         'en-us': enUS,
-//     },
-// });
-
-// export default i18n;
+export default {
+  'en-US': enUS
+};
